939b1f80fdf7743e79ffffd44cd1db082fa03482
Katie Fenn <katie.fenn@googlemail.com>
2015-07-05 19:58:52 +0100

    Complete first version of theme

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,10 +15,28 @@ module.exports = function(grunt) {
     },
     watch: {
       css: {
-        files: ['css/sass/*.scss','css/*.scss'],
-        tasks: ['default'],
-        options: {
-          livereload: true
+        files: ['assets/css/sass/**/*.scss','assets/css/*.scss'],
+        tasks: ['sass']
+      },
+      uglify: {
+        files: ['js/*.js'],
+        tasks: ['uglify']
+      }
+    },
+    svgcss: {
+      backgrounds: {
+        files: {
+          'assets/css/sass/_svg.scss': ['assets/img-source/*.svg']
+        }
+      }
+    },
+    uglify: {
+      options: {
+        compress: {}
+      },
+      default: {
+        files: {
+          'assets/dist/scripts.min.js': ['assets/js/*.js']
         }
       }
     }
@@ -26,12 +44,12 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-watch');
-
-  // Default task(s).
-  grunt.registerTask('default', ['sass']);
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-svg-css');
 
   // Server task
-  grunt.registerTask('build', ['sass']);
-  grunt.registerTask('server', ['build','watch']);
+  grunt.registerTask('build', ['sass', 'uglify']);
+  grunt.registerTask('build-images', ['svgcss']);
+  grunt.registerTask('dev', ['build','watch']);
 
 };
